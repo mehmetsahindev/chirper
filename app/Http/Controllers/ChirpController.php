870ea240b3b1c28a4905ef2460deca0e155ad1a1
root <root@mehmetsahin.dev>
2023-07-16 20:30:54 -0400

    laravel bootcamp completed

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() :View
+    {
+
+        return view("chirps.index", [
+            "chirps" => Chirp::with("user")->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) :RedirectResponse
+    {
+        $validated = $request->validate([
+            "message" => "required|string|max:255"
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route("chirps.index"));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @throws AuthorizationException
+     */
+    public function edit(Chirp $chirp) :View
+    {
+        $this->authorize('update', $chirp);
+
+        return view("chirps.edit", [
+            "chirp" => $chirp
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @throws AuthorizationException
+     */
+    public function update(Request $request, Chirp $chirp) :RedirectResponse
+    {
+
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @throws AuthorizationException
+     */
+    public function destroy(Chirp $chirp)
+    {
+        $this->authorize("delete", $chirp);
+
+        $chirp->delete();
+
+        return redirect(route("chirps.index"));
+    }
+}
